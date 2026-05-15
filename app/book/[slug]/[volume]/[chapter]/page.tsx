87539062f5134c53e7ad458eded0c24195f9db92
TaherAlmahdi <116091472+TaherAlmahdi@users.noreply.github.com>
2026-05-16 01:42:37 +0300

    Update page.tsx

--- app/book/[slug]/[volume]/[chapter]/page.tsx
+++ app/book/[slug]/[volume]/[chapter]/page.tsx
@@ -178,14 +178,10 @@ export default async function ChapterPage({ params, searchParams }: Props) {
           <span className="mx-1 text-white/50 shrink-0">/</span>
           <Link href={`/book/${slug}/${volume}`} className="hover:text-red-100 shrink-0">{currentVolTitle}</Link>
           <span className="mx-1 text-white/50 shrink-0">/</span>
-         <div className="font-medium flex flex-wrap gap-x-1">
-  <span>{chapData.title || chapter}</span>
-  {totalPages > 1 && (
-    <span className="shrink-0 text-gray-500">
-      (পৃষ্ঠা {toBengaliNumber(currentPage)})
-    </span>
-  )}
-</div>
+         <span className="font-medium flex flex-wrap break-words inline-block">
+  {chapData.title || chapter} {totalPages > 1 && `(পৃষ্ঠা ${toBengaliNumber(currentPage)})`}
+</span>
+
 
         </div>
       </nav>
