cc47b24e5213da5e333a5d6a0368ccb1aeb6588e
TaherAlmahdi <116091472+TaherAlmahdi@users.noreply.github.com>
2026-07-29 16:43:08 +0300

    Update AuthorList.tsx

--- app/components/AuthorList.tsx
+++ app/components/AuthorList.tsx
@@ -56,7 +56,7 @@ const AuthorList = async () => {
       <div className="relative z-20 w-full max-w-none mx-auto">
         {/* টাইটেল হেডার */}
         <div className="flex justify-center">
-          <div className="inline-flex items-center justify-center gap-3 px-5 py-2 rounded bg-teal-50/90 text-[#008080] mb-8 border border-teal-100 shadow-xs text-center backdrop-blur-md">
+          <div className="inline-flex items-center justify-center gap-3 px-2 py-2 rounded bg-teal-50/90 text-[#008080] mb-8 border border-teal-100 shadow-xs text-center backdrop-blur-md">
             <Users size={24} className="shrink-0 animate-pulse" />
             <h1 className="text-xl md:text-2xl font-tarunima font-black text-gray-900 leading-none tracking-tight">
               <span className="text-[#008080]">সম্মানিত</span> লেখক <span className="text-[#cc7a00]">তালিকা</span>
@@ -66,7 +66,7 @@ const AuthorList = async () => {
 
         {/* বর্ণানুক্রমে সাজানো লেখক তালিকা */}
         {authors.length === 0 ? (
-          <div className="text-center p-8 bg-white/80 rounded-xl text-gray-600 w-full">
+          <div className="text-center p-8 bg-white/80 rounded text-gray-600 w-full">
             কোনো লেখকের তথ্য পাওয়া যায়নি।
           </div>
         ) : (
@@ -75,7 +75,7 @@ const AuthorList = async () => {
               <Link
                 key={slug}
                 href={`https://library.eduliture.org/${slug}`}
-                className="flex items-center justify-between gap-3 px-4 py-3 rounded mb-1 bg-white/90 text-[#008080] border border-teal-100 shadow-sm transition-all duration-300 backdrop-blur-sm hover:bg-teal-50 hover:shadow-lg hover:border-teal-300 hover:scale-[1.02] shrink-0 grow basis-full sm:basis-[calc(50%-0.75rem)] lg:basis-[calc(33.333%-0.75rem)] xl:basis-[calc(25%-0.75rem)] 2xl:basis-[calc(20%-0.75rem)] max-w-full group cursor-pointer overflow-hidden"
+                className="flex items-center justify-between gap-3 px-2 py-3 rounded mb-1 bg-white/90 text-[#008080] border border-teal-100 shadow-sm transition-all duration-300 backdrop-blur-sm hover:bg-teal-50 hover:shadow-lg hover:border-teal-300 hover:scale-[1.02] shrink-0 grow basis-full sm:basis-[calc(50%-0.5rem)] lg:basis-[calc(33.333%-0.75rem)] xl:basis-[calc(25%-0.75rem)] 2xl:basis-[calc(20%-0.75rem)] max-w-full group cursor-pointer overflow-hidden"
               >
                 {/* বামপাশে লেখকের সিঙ্গেল ইউজার আইকন ও নাম */}
                 <div className="flex items-center gap-3 min-w-0">
