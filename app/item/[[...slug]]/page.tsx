39e529d8da4315e099dd3c728e1b07bc9cc6ee9c
TaherAlmahdi <116091472+TaherAlmahdi@users.noreply.github.com>
2026-08-31 18:18:45 +0300

    Update page.tsx

--- app/item/[[...slug]]/page.tsx
+++ app/item/[[...slug]]/page.tsx
@@ -140,6 +140,7 @@ const getItemTypeDisplayName = (itemObj: any): string => {
     poem: 'কবিতা',
     books: 'বই',
     articles: 'প্রবন্ধ',
+letter: 'চিঠি'
   };
   return typeMap[typeSlug] || typeSlug || 'প্রকরণ';
 };
